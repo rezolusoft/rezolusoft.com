dd474a180b639e23a08ddf78df5b7c99987ffd95
baba-mandef <contact@abiodoun.dev>
2024-08-10 01:05:41 +0100

    ➕ add prod settings

---
+++ root/prod.py
@@ -0,0 +1,2 @@
+from .settings import *
+STATIC_ROOT = os.path.join('/home/rezogsyk/rezolusoft/static/')
--- root/settings.py
+++ root/settings.py
@@ -134,9 +134,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static/'),
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
