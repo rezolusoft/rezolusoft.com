57450d348ca53f71348bb6f6a14da9b4db807686
baba-mandef <contact@abiodoun.dev>
2024-08-16 09:23:27 +0100

    📝 update service

--- rezolusoft/service/models/service.py
+++ rezolusoft/service/models/service.py
@@ -1,5 +1,16 @@
 from django.db import models
+from rezolusoft.service.models import Category
+from tinymce import models as tinymce_models
 
 class Service(models.Model):
     name = models.CharField(max_length=100)
-    description = models.TextField()
+    code = models.CharField(max_length=50, blank=True, db_index=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    description = models.CharField(max_length=500)
+    icon = models.CharField(max_length=50)
+    illustration = models.CharField(max_length=50)
+    info = tinymce_models.HTMLField(blank=True)
+
+    def __str__(self) -> str:
+        return f'{self.name}'
+    
