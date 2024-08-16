33b8493230d47275efeff386e01487ab9abc5d81
baba-mandef <contact@abiodoun.dev>
2024-08-16 06:36:45 +0100

    📝 update url conf

--- rezolusoft/home/urls.py
+++ rezolusoft/home/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from rezolusoft.home.views import HomePage
+from rezolusoft.home.views import (HomePage, ServiceCategoryDetailsView)
 
 urlpatterns = [
-    path('', HomePage.as_view(), name='home' )
+    path('', HomePage.as_view(), name='home' ),
+    path("services/<str:category>/", ServiceCategoryDetailsView.as_view(), name='services')
     
 ]
