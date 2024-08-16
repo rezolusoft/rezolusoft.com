59ab8c000d0fdb8dfee3deff3bcfa197e5ffd32c
baba-mandef <contact@abiodoun.dev>
2024-08-16 09:34:56 +0100

    📝 update category model

--- rezolusoft/service/models/category.py
+++ rezolusoft/service/models/category.py
@@ -1,12 +1,14 @@
 from django.db import models
+from tinymce import models as tinymce_models
 
 
-class ServiceCategory(models.Model):
+class Category(models.Model):
 
-    title  = models.CharField(max_length=150)
+    code  = models.CharField(max_length=150, unique=True, db_index=True)
+    name = models.CharField(max_length=150)
     description = models.CharField(max_length=500)
-    icon = models.ImageField(upload_to="service/category/")
+    info = tinymce_models.HTMLField(blank=True)
 
 
     def __str__(self) -> str:
-        return self.title
+        return self.name
