f4e7c63727d176b87cd317082b48fca72fd6e2ba
baba-mandef <contact@abiodoun.dev>
2024-08-16 09:35:14 +0100

    📝 register models

--- rezolusoft/service/admin.py
+++ rezolusoft/service/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from rezolusoft.service.models import Service, Category
 
 # Register your models here.
+
+admin.site.register(Service)
+admin.site.register(Category)
