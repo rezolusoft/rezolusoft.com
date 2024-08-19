570d8259b4c1f55353e9b27e1e2753d3dadf5995
baba-mandef <contact@abiodoun.dev>
2024-08-19 18:41:00 +0100

    📝 add latest work fetching

--- rezolusoft/home/views/home_page.py
+++ rezolusoft/home/views/home_page.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import View
 from rezolusoft.service.models import Category
+from rezolusoft.work.models import Work
 
 
 class HomePage(View):
@@ -8,8 +9,10 @@ class HomePage(View):
     def get(self, request):
 
         categories = Category.objects.all()
+        works = Work.objects.all()[:3]
         context = {
             'categories': categories,
+            'works': works
         }
 
         return render(request, 'pages/home.html', context=context)
