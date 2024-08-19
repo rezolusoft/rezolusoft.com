87c2e5d3857d3ee2d84ecd21780ddff4b022a80a
baba-mandef <contact@abiodoun.dev>
2024-08-19 18:30:10 +0100

    📝 add work url config

--- rezolusoft/home/urls.py
+++ rezolusoft/home/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from rezolusoft.home.views import (HomePage, ServiceCategoryDetailsView)
+from rezolusoft.home.views import (HomePage, WorkListView, WorkDetailsView, ServiceCategoryDetailsView)
 
 urlpatterns = [
     path('', HomePage.as_view(), name='home' ),
-    path("services/<str:category>/", ServiceCategoryDetailsView.as_view(), name='services')
-    
+    path("services/<str:category>/", ServiceCategoryDetailsView.as_view(), name='services'),
+    path('work', WorkListView.as_view(), name='work' ),
+    path('work/<str:slug>/', WorkDetailsView.as_view(), name='work-details' ),  
 ]
